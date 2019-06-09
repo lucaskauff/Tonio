33c4ab86d0566512917dafa0c8e885b6c3fb5afe
Lucas Kauff <l.kauff@rubika-edu.com>
2019-06-10 00:47:06 +0200

    Oh, you can save your progress !

---
+++ Tonio/Assets/1_Scripts/SavingSystem/SimpleSaveTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tonio
+{
+    public class SimpleSaveTest : MonoBehaviour
+    {
+        InputManager inputManager;
+
+        private void Start()
+        {
+            inputManager = GameManager.Instance.inputManager;
+
+            transform.position = ES3.Load<Vector3>("tonioPosition", Vector3.zero);
+        }
+
+        void Update()
+        {
+            if (inputManager.secretSaveKey)
+            {
+                ES3.Save<Vector3>("tonioPosition", transform.position);
+            }
+
+            if (inputManager.secretDeleteKey1)
+            {
+                ES3.DeleteKey("tonioPosition");
+            }
+        }
+    }
+}
