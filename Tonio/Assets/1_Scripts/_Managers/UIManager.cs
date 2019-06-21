6170f0e85bacab0e5bc7da8b45f9f5135d617894
Lucas Kauff <l.kauff@rubika-edu.com>
2019-06-21 10:11:47 +0200

    UIManager started

---
+++ Tonio/Assets/1_Scripts/_Managers/UIManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Tonio
+{
+    public class UIManager : MonoBehaviour
+    {
+        //GameManager
+        GameManager gameManager;
+
+        [Header("UI Elements")]
+        [SerializeField] Image cooldownFeedbackFill = default;
+        [SerializeField] Animator cooldownFeedbackAnim = default;
+
+        //Private   
+        bool baguetteIsThere = false;
+        float cdFill;
+
+        private void Start()
+        {
+            gameManager = GameManager.Instance;
+
+            if (gameManager.powerIsReady)
+            {
+                MakeTheBaguetteAppear();
+            }
+        }
+
+        private void Update()
+        {
+            if (!baguetteIsThere && gameManager.powerIsReady)
+            {
+                MakeTheBaguetteAppear();
+            }
+
+
+        }
+
+        public void MakeTheBaguetteAppear()
+        {
+            cooldownFeedbackAnim.SetTrigger("Appear");
+            baguetteIsThere = true;
+        }
+    }
+}
