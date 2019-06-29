86c685192b5e7565ab553cc3ece6d1b4aaa6935c
Lucas Kauff <l.kauff@rubika-edu.com>
2019-06-29 14:13:17 +0200

    Tilemap in progress

--- Tonio/Assets/1_Scripts/GridScripts/HidingTiles.cs
+++ Tonio/Assets/1_Scripts/GridScripts/HidingTiles.cs
@@ -6,6 +6,12 @@ namespace Tonio
 {
     public class HidingTiles : MonoBehaviour
     {
+        [Header("My components")]
+        [SerializeField] Collider2D myCol = default;
+
+        [Header("Other objects to serialize")]
+        [SerializeField] GameObject theRenderingOfTilemapCol = default;
+        [SerializeField] UIManager uIManager = default;
         [SerializeField] HouseDoorFunctionning theDoor = default;
 
         private void OnTriggerEnter2D(Collider2D collision)
