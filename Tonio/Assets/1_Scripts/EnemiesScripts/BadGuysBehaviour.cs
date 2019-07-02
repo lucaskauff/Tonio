56e351911e0a8b55b376f71f3781604f673d2986
Lucas Kauff <l.kauff@rubika-edu.com>
2019-07-02 20:05:27 +0200

    Animator is a mess

--- Tonio/Assets/1_Scripts/EnemiesScripts/BadGuysBehaviour.cs
+++ Tonio/Assets/1_Scripts/EnemiesScripts/BadGuysBehaviour.cs
@@ -14,22 +14,83 @@ namespace Tonio
 
         [Header("To serialize")]
         [SerializeField] bool isBerto = true;
+        [SerializeField] float moveSpeed = 1;
         [SerializeField] Transform[] movePoints;
 
         //Public
         [HideInInspector] public bool shouldArrive = false;
 
+        //Private
+        bool shouldGo = false;
+        bool isArrivedInHouse = false;
+        bool isArrivedOutside = false;
+        int target = 1;
+
+        private void Start()
+        {
+            myAnim.SetBool("IsBerto", isBerto);
+        }
+
         private void Update()
         {
             if (shouldArrive)
             {
-                BadGuysComingInTheHouse();
+                EnterHouse();
+            }
+            else if (isArrivedInHouse)
+            {
+                LookAroundInHouse();
+            }
+            else if (shouldGo)
+            {
+                ExitHouse();
+            }
+            else if (isArrivedOutside)
+            {
+                LookOutsideHouse();
             }
         }
 
-        void BadGuysComingInTheHouse()
+        void EnterHouse()
+        {
+            MakeAMove(movePoints[target].position);
+
+            if (target == 1)
+            {
+                myAnim.SetTrigger("WalkNorth");
+
+                if (transform.position == movePoints[target].position)
+                {
+                    target = 0;
+                }
+            }
+            else if (target == 0)
+            {
+                myAnim.SetTrigger("WalkLeft");
+
+                shouldArrive = false;
+                isArrivedInHouse = true;
+            }
+        }
+
+        void LookAroundInHouse()
         {
 
+        }
+
+        void ExitHouse()
+        {
+
+        }
+
+        void LookOutsideHouse()
+        {
+
+        }
+
+        void MakeAMove(Vector3 pointWhereToGo)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, pointWhereToGo, moveSpeed * Time.deltaTime);
         }
     }
 }
